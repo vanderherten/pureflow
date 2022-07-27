c5e5939e1819f1d67437be1e03a5a3375d4aaa76
Anne-Marie Van der herten <avanderherten@gmail.com>
2022-07-27 16:45:12 +0000

    Add toggle function to mobile navigation button with javascript

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// MOBILE FIRST
+
+// Toggle function Mobile Navigation Button
+
+const navEl = document.querySelector('nav');
+const navBtnEl = document.querySelector('.nav__mobile-btn');
+
+navBtnEl.addEventListener('click', () => {
+  navEl.classList.toggle('nav--open');
+});
